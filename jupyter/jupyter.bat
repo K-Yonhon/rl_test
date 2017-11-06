1f3631466dee147ad2dc2d37eb226b0e67e3211a
wakiyama shinichi <wakiyama@mail.com>
2017-11-06 17:34:27 +0900

    add ipynb

---
+++ jupyter/jupyter.bat
@@ -0,0 +1,3 @@
+@echo off
+rem C:\Anaconda3\python.exe C:\Anaconda3\cwp.py C:\Anaconda3 "C:/Anaconda3/python.exe" "C:/Anaconda3/Scripts/jupyter-notebook-script.py" --no-browser %~dp0
+C:\Anaconda3\python.exe C:\Anaconda3\cwp.py C:\Anaconda3 "C:/Anaconda3/python.exe" "C:/Anaconda3/Scripts/jupyter-lab-script.py" --no-browser %~dp0
