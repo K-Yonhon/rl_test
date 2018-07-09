773d96af95b3bfdaee8a67ebf409f8e07762bf9e
y836475809 <tttdaytttday@gmail.com>
2018-07-10 01:14:45 +0900

    整理

--- maze/mz2.py
+++ maze/mz2.py
@@ -33,12 +33,6 @@ from keras.backend import tensorflow_backend, set_session
 config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 session = tf.Session(config=config)
 tensorflow_backend.set_session(session)
-# config = tf.ConfigProto(
-#     gpu_options=tf.GPUOptions(
-#         visible_device_list="0", # specify GPU number
-#         allow_growth=True
-#     )
-# )
 set_session(tf.Session(config=config))
 
 os.environ['PYTHONHASHSEED'] = '0'
@@ -64,7 +58,6 @@ class Mz(gym.core.Env):
 
         self.maze = np.ones(mz_data.shape)
 
-        # self.pos = [0, 0]
         self.st = None
         self.actions = {
             0: (0, 1),
@@ -80,17 +73,10 @@ class Mz(gym.core.Env):
 
         self.check_flags = self.flags.copy()
 
-        # self.flg_data = np.ones((len(self.flags), ))
-        # flg_pos_list = [[row, col] for row, col in self.flags]
-        # self.flg_pos = np.array(flg_pos_list)
         self.flg_dict = {}
         for i, flg_pos in enumerate(self.flags):
             self.flg_dict[tuple(flg_pos)] = i
 
-        # low = np.empty(self.maze.shape)
-        # high = np.empty(self.maze.shape)
-        # low.fill(self.AGENT)
-        # high.fill(self.PATH)
         low = self.WALL
         high = self.PATH
         self.observation_space = gym.spaces.Box(low=low, high=high, shape=self.maze.shape, dtype=np.float32)
@@ -102,34 +88,9 @@ class Mz(gym.core.Env):
         mv_x, mv_y = self.actions[action]
 
         cu_pos = [self.pos[0] + mv_y, self.pos[1] + mv_x]
-        # self.pos = [self.pos[0] + mv_y, self.pos[1] + mv_x]
-        # reward = -0.1
-        bl = False
+
         inv_re = -1.5
         if cu_pos in self.walls:
-            bl = True
-        # else:
-        #     if self.pos[0] >= self.c_size:
-        #         self.pos[0] = self.c_size-1
-        #         bl = True
-        #         # self.up_mz()
-        #         # return self.maze, inv_re, False, {}
-        #     if self.pos[0] < 0:
-        #         self.pos[0] = 0
-        #         bl = True
-        #         # self.up_mz()
-        #         # return self.maze, inv_re, False, {}
-        #     if self.pos[1] >= self.r_size:
-        #         self.pos[1] = self.r_size-1
-        #         bl = True
-        #         # self.up_mz()
-        #         # return self.maze, inv_re, False, {}
-        #     if self.pos[1] < 0:
-        #         self.pos[1] = 0
-        #         bl = True
-        #         # self.up_mz()
-        #         # return self.maze, inv_re, False, {}
-        if bl:
             self.bl_cnt += 1
             self.up_mz()
             return self.maze, inv_re, False, {}
@@ -175,14 +136,9 @@ class Mz(gym.core.Env):
         self.pos = np.copy(self.start)
         self.goal = [self.max_index, self.max_index]
 
-        # self.maze.fill(self.PATH)
-        # self.maze[tuple(self.start)] = self.AGENT
-        # for fg in self.flags:
-        #     self.maze[tuple(fg)] = self.FLAG
         self.up_mz()
 
         self.check_flags = self.flags.copy()
-        # self.flg_data = np.ones((len(self.flags),))
 
         self.st = np.copy(self.maze)
         return self.st
@@ -200,23 +156,11 @@ class Mz(gym.core.Env):
         # pil_img = Image.fromarray(self.maze*255.).convert("L")
         # pil_img.save('./tmp/lenna_changed_{0}.png'.format(self.cnt))
 
-        self.cnt+=1
+        self.cnt += 1
 
 
 # m_size = 10
 channel = 4
-
-# class MazeProcessor(Processor):
-#     def __init__(self):
-#         self.rgb_state = np.zeros((m_size, m_size, channel))
-#
-#     def process_step(self, observation, reward, done, info):
-#         old_rgb_state = self.rgb_state.copy()
-#         self.rgb_state[:, :, 0] = observation.flatten()
-#         for i in range(1, channel):
-#             self.rgb_state[:, :, i] = old_rgb_state[:, :, i-1]
-#
-#         return self.rgb_state, reward, done, info
 
 
 def main():
@@ -225,35 +169,7 @@ def main():
     env.seed(7)
     nb_actions = env.action_space.n
     m_size = env.c_size
-    # n_szie = env.a_self
-    # n_szie = 500
-    ac = 'relu'
-    # ac = 'sigmoid'
-    # ac = 'tanh'
-    a = 0.24
-    # model = Sequential()
-    # model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-    #
-    # model.add(Dense(n_szie))
-    # # # # model.add(Dense(n_szie, input_shape=(1, n_szie)))
-    # # # # model.add(Activation(ac))
-    # model.add(LeakyReLU(alpha=a))
-    # # model.add(Dropout(drop))
-    # model.add(BatchNormalization())
-    #
-    # model.add(Dense(n_szie))
-    # # model.add(Activation(ac))
-    # model.add(LeakyReLU(alpha=a))
-    # # model.add(Dropout(drop))
-    # model.add(BatchNormalization())
-    #
-    # model.add(Dense(nb_actions))
-    # # model.add(Activation('softmax'))
-    # # model.add(Activation('relu'))
-    # model.add(LeakyReLU(alpha=a))
-    # # model.add(Dense(1, activation='linear'))
-    # # model.add(Activation('linear'))
-    # print(model.summary())
+
     n_filters = 4
     kernel = (3, 3)
     strides = (1, 1)
@@ -274,7 +190,6 @@ def main():
     model.add(Dense(nb_actions, activation="relu"))
     print(model.summary())
 
-    # processor = MazeProcessor()
     # experience replay用のmemory
     memory = SequentialMemory(limit=5000, window_length=channel)
     # 行動方策はオーソドックスなepsilon-greedy。ほかに、各行動のQ値によって確率を決定するBoltzmannQPolicyが利用可能
@@ -283,14 +198,12 @@ def main():
     dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
                    enable_dueling_network=True, dueling_type='avg', target_model_update=1e-2,
                    policy=policy)
-                   # processor=processor)
     dqn.compile(Adam(lr=1e-3, clipnorm=1.), metrics=['mae'])
 
     tb = TensorBoard()
-    history = dqn.fit(env, nb_steps=1000000, visualize=False, verbose=2, nb_max_episode_steps=400,
+    history = dqn.fit(env, nb_steps=1000000, visualize=False, verbose=2, nb_max_episode_steps=1000,
                       callbacks=[tb])
 
 
 if __name__ == '__main__':
     main()
-    # test()
